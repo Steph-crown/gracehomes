7722f71b09efabe5d3dedba3a65c35e262fa0061
Stephen Emmanuel <emmanuelstephen024@gmail.com>
2020-06-17 21:30:18 +0100

    Add files via upload

---
+++ buys.js
@@ -0,0 +1,240 @@
+function $(x) { return document.getElementById(x)}
+buys = [ 
+		 {  
+			name:"Five Oaks Residence",
+			img:"f2.jpg",
+			locate:"Lekki-Epe Expressway",
+			text:"When you are thinking about owing a real estate investment that the return on investment is inevitable within a while. Think of Five Oaks Residences.<br>"+
+			"<b>Why Five Oaks Residences?</b><br>"+
+			"<ul>"+
+			"<li>It’s strategically located 1 minute off the Lekki Epe Expressway</li>"+
+			"<li>it’s beside the popular <em>JIU HUA GROUP</em> Eleko</li>"+
+			"<li>Directly facing the Eleko road before Amen Estate</li>"+
+			"<li>It’s a ready to build estate</li>"+
+			"<li>Close proximity to eleko beach and the lekki free zone</li>"+
+			"<li>Beautiful estate with good layout design</li>"+
+			"</ul><br/>"+
+			"<b>INFRASTRUCTURES</b>"+
+			"<ul>"+
+			"<li>Drainage system</li>"+
+			"<li>Electricity/Electric Poles</li>"+
+			"<li>Security Cams</li>"+
+			"<li>Street Lightings</li>"+
+			"<li>Interlocked Road network. Recreational Facilities</li>"+
+			"<li>Perimeter Fencing</li>"+
+			
+			"<b>Title: Governor’s Consent </b><br"+
+			"<b>Price</b>"+
+			"<ul>"+
+			"<li>600sqm: N13,000,000</li>"+
+			"<li>300sqm: N6,500,000</li>"+
+			"</ul>"+
+			"<em>For more information and inquiries call XXXXXXXXXXX</em>"
+		  },
+		  {
+		    name:"Genesis Court",
+		    img:"f3.jpg",
+		    locate:"Ajah, Lagos",
+		    text:"<b>A Serene Residential Estate, In The Heart of Lagos (Ajah)</b><br>"+
+		    "<em>Genesis Court</em> is a well managed residential estate located in the Ajah area of Lagos State.<br>"+
+		    "The estate is a joint venture project between <em>Urban Housing Cooperative of Lagos State</em> (owners of Cooperative Villas Estate), <em>First World Communities Ltd. (FWC Ltd.)</em> and <em>Neighbourhood Review Ltd</em>.<br/>"+
+		    "This is one of the best locations you can even imagine to live with your family and there are lands of various sizes available for:"+
+		    "<ul>"+
+		    "<li><em>Security & Peace of Mind</em><br>"+
+		    "After the traffic and hustle that comes with living in the heart of Lagos, it’s ideal to return to a home & community that offers security, peace, serenity, facilities and ambiance comparable to living standards in developed countries.<br>"+
+		    "Everything You Need In One Place</li>"+
+		    "<li><em>Location / Accessibility</em></br>"+
+		    "Genesis Court is located in Badore, Ajah; the diversion left at the Ajah Flyover Bridge (Ajah Bus Stop). It is located just beside the popular Cooperative Villas Estate. The estate is   along a tarred government road, and the surrounding neigbourhood is fully built up and serene."+
+		    "<li><em>Civilized Neighbours</em><br/>"+
+		    "From the estate design and building / development control regulations, it’s clear that Genesis Court is designed to be a well organized, safe and serene neighbourhood within the heart of Lagos; that offers middle and upper class Lagosians an opportunity to reside among like minded people. </li>"+
+		    "<li><em>Infrastructure & Facilities</em><br/>"+
+		    "The estate is a well managed serviced estate, with quality facilities & infrastructure already in place.<br>"+
+		    "The estate management is also open to introducing a central generator(via an IPP) that will provide guaranteed 24/7 power to residents, once the number of resident families makes that economically viable.</li>"+
+		    "<b>Infrastructure in place include:</b>"+
+		    "<ul>"+
+		    "<li>Well Interlocked Roads & Walkways</li>"+
+		    "<li>Effective Drainage Network</li>"+
+		    "<li>Estate Recreation Park (Sit out) & Children’s Playground</li>"+
+		    "<li>Horticulture (Well maintained Trees & Flowers)</li>"+
+		    "<li>Electricity Infrastructure, with Dedicated 300Kva Transformer</li>"+
+		    "<li>Solar Street Lights</li>"+
+		    "<li>Private Uniformed Security in Addition to Mobile Police</li>"+
+		    "<li>CCTV Surveillance</li>"+
+		    "</ul>"+
+		    "<b>What You Can Build !</b><br>"+
+		    "<em>Detached Duplexes</em>"+
+		    "The estate building control regulations permit fully detached duplexes on any land size, starting from 400sqm; which is the minimum plot size in the estate.<br/>"+
+		    "<em>Semi-Detached Duplexes</em>"+
+		    "The estate building control regulations permit Semi-detached duplexes on plots from 500 Sqm Upward. <br/>"+
+		    "<em>Terraced Duplexes</em>"+
+		    "The estate building control regulations permit terraced duplexes on large plots, so far the necessary regulatory set back requirements are satisfied, and each unit has parking space for at least 2 cars."+
+		    "<br><em>Block of Flats</em>"+
+		    "The estate has specific large plots at the end sections of each road, that are specially set aside for development of blocks of apartment . Subscribers can purchase these plots and build apartment blocks, but also have to ensure necessary regulatory set back and parking space requirements are satisfied."+
+		    "<br><b>Affordable Payment Plans for Any Budget</b>"+
+		    "<br><em>Outright Payment : ₦30,000/Sqm</em><br>"+
+		    "<em>NGN12,000,000  for 400sqm</em>"+
+		    "<br><em>Development Fee: ₦5,000 / Sqm</em>"+
+		    "<br><em>Survey Fee: ₦1,000,000</em>"+
+		    "<br><em>Legal / Documentation Fee: ₦400,000</em>"+
+		    "<br><b>Any plot size can be purchased based on the budget and buyer’s preference</b>"+
+		    "<br><b>3 Months Installment</b>"+
+		    "<br><em>₦30,750/Sqm</em>"+
+		    "<br><em>NGN12,300,000 for 400sqm</em>"+
+		    "<br><em>Development Fee: ₦5,000 / Sqm</em>"+
+		    "<br><em>Survey Fee: ₦1,000,000</em>"+
+		    "<br><em>Legal / Documentation Fee: ₦400,000</em><br>"+
+		    "<br><b>6 Months Installment</b>"+
+		    "<br><em>₦31,500/Sqm</em>"+
+		    "<br><em>NGN12,600,000 for 400sqm</em>"+
+		    "<br><emDevelopment Fee: ₦5,000 / Sqm</em>"+
+		    "<em>Survey Fee: ₦1,000,000</em>"+
+		    "<br><em>Legal / Documentation Fee: ₦400,000</em>"+
+		    "For more information Call XXXXXXXXXX"
+		  },
+		  {
+		    name:"Lavadia Urban Prime Two Estate",
+		    img:"f4.jpg",
+		    locate:"Abraham Adesanya",
+		    text:"Lavadia is an arcade of magnificent surroundings and exceptional estate living, a stunning home destination for every resident who seeks uppermost tranquillity and clean aesthetics. It is the ultimate spot for rest and restoration."+
+		    "<br><em>Location</em>: Abraham Adesanya<br>"+
+		    "<em>Title</em>: Governor’s Consent"+
+		    "<br><em>Units</em>: Spacious and well ventilated 2&3 bedroom terraces and 3&4 semi-detached duplexes [all fully finished]<br>"+
+		    "<em>Payment Plan</em>: Up to 18 months<br>"+
+		    "<em>Initial deposit</em>: N2.99M<br>"+
+		    "<b>Prices</b>:<br>"+
+		    "👉  🏽2 Bedroom Terrace - N35m<br>"+
+		    "👉  🏽3 Bedroom Terrace - N38m <br>"+
+		    "👉  🏽3 Bedroom Semi-Detached - N43m<br>"+
+		    "👉  🏽4 Bedroom Semi-Detached - N55m<br>"+
+		    "<b>Neighbourhood:</b>"+
+		    "<ul>"+
+		    "<li>Lekki Scheme 2</li>"+
+		    "<li>Inoyo Havens</li>"+
+		    "<li>Abraham Adesanya Estate</li>"+
+		    "<li>Chaplain Court</li>"+
+		    "<li>Lagos Business School</li>"+
+		    "<li>Caleb British International School</li>"+
+		    "<li>Atican Beach</li>"+
+		    "<li>Barracuda Beach</li>"+
+		    "<li>Lekki Miami Beach Resort</li>"+
+		    "<li>MX Film and Multimedia Academy</li>"+
+		    "<li>Novare Mall</li>"+
+		    "<li>Lufasi Nature Park</li>"+
+		    "With Urban Prime Two, we welcome you to taste the finer side of home.<br>"+
+		    "Urban Prime Two: Own Something Timeless....*"
+		 },
+		 { 
+		    name:"Gracias Sunstone Residence",
+		    img:"f5.jpg",
+		    locate:"Ibeju-Lekki, Lagos",
+		    text:"Location Ibeju-Lekki Lagos, few minutes from the lekki free trade zone where massive development is on going at the moment."+
+		    "It's a 5 years payment plan with a very flexible payment pattern.<br>"+
+		    "<br><b>3 Years Plan</b>"+
+		    "<br><em>1 Bedroom: N10.5m</em>"+
+		    "<br><em>2 Bedroom: N11.5m</em>"+
+		    "<br><b>5 Years Plan</b>"+
+		    "<br><em>1bedroom: N12.5m</em>"+
+		    "<br><em>2bedroom: N13.5m</em>"+
+		    "<br><br><b>Pay N250,000 or N500,000 for 1 bedroom or 2 bedroom and spread the payment for 5 years.<br>"+
+		    "Isn’t that a great deal for you?😂</b>"+
+		    "<br><br><b>3 BEDROOM FLAT STYLE A</b><br>"+
+		    "Initial deposit N500,000 or N1,000,000 and spread the balance<br>"+
+		    "<em>N20m for 3 years </em>"+
+		    "<br><em>N21m for 4 years</em>"+
+		    "<br><em>N22m for 5 years</em>"+
+		    "<br><br><b>3 BEDROOM FLAT STYLE B</b>"+
+		    "Initial deposit N500,000 or N1,000,000 and spread the balance"+
+		    "<br><em>N13m for 3 years</em>"+
+		    "<br><em>N14m for 4 years</em>"+
+		    "<br><em>N15m for 5 years</em>"+
+		    "<br><br><b>3 BEDROOM TERRACE DUPLEX WITH BQ</b><br>"+
+		    "Initial deposit N500,000 or N1,000,000 and spread the balance"+
+		    "<br><em>N23m for 3 years</em>"+
+		    "<br><em>N24m for 4 years</em>"+
+		    "<br><em>N25m for 5 years</em>"+
+		    "<br><br><b>INFRASTRUCTURE</b>"+
+		    "<ul>"+
+		    "<li>Accessible Road</li>"+
+		    "<li>Perimeter Fencing</li>"+
+		    "<li>Water Connection </li>"+
+		    "<li>Electricity Connection</li>"+
+		    "<li>Street Light</li>"+
+		    "<li>CCTV Camera </li>"+
+		    "<li>Drainage System </li>"+
+		    "<li>Sanitary Sewer System</li>"+
+		    "<li>24 Hours Electricity</li>"+
+		    "</ul>"+
+		    "<br><b>LANDMARKS</b><br>"+
+		    "<ul>"+
+		    "<li>Lekki Free trade zone</li>"+
+		    "<li>Dangote Refinery</li>"+
+		    "<li>Lekki Deep Sea port</li>"+
+		    "<li>Pan African University</li>"+
+		    "<li>LA campaigne Tropicanal Resort</li>"+
+		    "And other several gated estates.<br>"+
+		    "For more information and inquiries ☎️ XXXXXXXXXXX"
+		  },
+		  {
+		    name:"Berry Court",
+		    img:"f6.jpg",
+		    locate:"Magodo Phase II",
+		    text:"Berry Court we present to you a place of abode to great serenity and development with higher demand due to its access to the business district of Lagos State.<br>"+
+		    "Its 100% dry and ready to build<br>"+
+		    "<em>Location</em>: Omole Phase II Extension Sharing Boundary with Magodo Phase II<br>"+
+		    "<em>Title</em>: C of O<br>"+
+		    "<em>Price</em>: NGN21,000,000<br>"+
+		    "<em>Size</em>: 500sqm<br>"+
+		    "<b>NEIGBOURHOOD</b><br>"+
+		    "- Omole Phase two<br>"+
+		    "- Magodo GRA Phase two<br>"+
+		    "- Ikeja City Mall<br>"+
+		    "- Lagos State Secretariat<br>"+
+		    "<b>BERRY COURT FACILITIES</b><br>"+
+		    "<ul>"+
+		    "<li>Perimeter Fence</li>"+
+		    "<li>Gate House</li>"+
+		    "<li>Security personnel</li>"+
+		    "<li>Power Supply</li>"+
+		    "<li>Good road network (with interlocking stones)</li>"+
+		    "<li>Drainage System</li>"+
+		    "<li>Water supply facilities</li>"+
+		    "<li>Recreational Centre.</li>"+
+		    "This is a diamond in the market.<br>"+
+		    "Remember the good things don’t stay long in the market.<br>"+
+		    "You only have now. Time doesn’t wait, when time runs the value follows, when the value increases the price skyrockets and finally it gets sold out. So the best time for you to make the call is now.<br>"+
+		    "Indulge—in so much excitement, for so little!"
+		  }
+		]
+buyMarkup = ""
+for (var i of buys) {	  
+  text = [i.text]
+  buyMarkup += "<div class='forSale' id='2'>" +
+						`<img src='${i.img}'>`+
+					"<div>"+
+						"<h3>" + i.name + "</h3>"+
+						"<p><i class='fa fa-map-marker'></i>" + i.locate + "</p><hr>"+
+						"<section class='tests'>" + i.text.slice(0,100) +"...</section>"+
+						"<span class='span'>...View More</span>"+
+						"<section class='newTests'></section>"+
+					"</div>"+
+				"</div>";
+}
+$('pro').innerHTML = buyMarkup;
+tests = document.getElementsByClassName('tests')
+newTests = document.getElementsByClassName('newTests')
+spans = document.getElementsByClassName('span')
+function viewMore(i) {
+  //alert(spans[i].innerHTML)
+  if (spans[i].innerHTML == "...View More") {
+    tests[i].innerHTML = ""
+    spans[i].innerHTML = "View Less"
+    newTests[i].innerHTML = buys[i].text
+  }
+  else {
+    tests[i].innerHTML = buys[i].text.slice(0,100);
+    spans[i].innerHTML = '...View More'
+    newTests[i].innerHTML = ""
+  }
+}
+_ = i => {spans[i].addEventListener('click', function(){viewMore(i)}, false)};
+for (var i = 0; i<spans.length; i++) _(i);
